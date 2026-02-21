f8591a6f88992d9477c8881de931499fdcfd8e42
Martin John M <johnmmartin74@gmail.com>
2026-02-21 12:53:18 +0100

    created a basic matchine engine

--- src/orderbook/orderbook.rs
+++ src/orderbook/orderbook.rs
@@ -2,7 +2,7 @@
 
 
 
-use crate::models::{Order, Trade, Side};
+use crate::models::{Order, Side, Trade};
 
 pub struct OrderBook {
     buy : Vec<Order>, // sorted descending by price
@@ -10,15 +10,46 @@ pub struct OrderBook {
 }
 
 impl OrderBook {
-    pub fn new() {
-
+    pub fn new() -> Self {
+        Self { buy: vec![], sell: vec![] }
     }
 
-    pub fn add_order() {
+    pub fn add_order(&mut self, order: Order) {
+        let book = match order.side {
+            Side::Buy => &mut self.buy,
+            Side::Sell => &mut self.sell
+        };
 
-    }
-
-    pub fn match_orders() {
+        book.push(order);
         
+        // Sort arrays 
+        self.buy.sort_by(|a,b| b.price.partial_cmp(&a.price).unwrap());
+        self.sell.sort_by(|a,b| a.price.partial_cmp(&b.price).unwrap());
+
+    }
+
+    pub fn match_orders(&mut self) -> Vec<Trade> {
+        let mut trades = vec![];
+
+        while !self.buy.is_empty() && !self.sell.is_empty() && self.buy[0].price >= self.sell[0].price {
+            let mut buy_order = self.buy.remove(0);
+            let mut sell_order = self.sell.remove(0);
+            let qty = buy_order.quantity.min(sell_order.quantity);
+
+            trades.push(Trade { 
+                buy_order_id: buy_order.id, 
+                sell_order_id: sell_order.id, 
+                quantity: qty, 
+                price: sell_order.price 
+            });
+
+            buy_order.quantity -= qty;
+            sell_order.quantity -= qty;
+
+            if buy_order.quantity > 0 { self.buy.insert(0, buy_order); }
+            if sell_order.quantity > 0 { self.sell.insert(0, sell_order); }
+
+        }
+        trades
     }
 }
