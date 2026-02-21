0849e0b119c125596106dc40f0dd9ebad115ad96
Martin John M <johnmmartin74@gmail.com>
2026-02-21 12:59:58 +0100

    Created sample trades from main file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 
 
 use orderbook_rust::models::{Order,Side};
+use orderbook_rust::orderbook::OrderBook;
 
 fn main() {
 
+    let mut book = OrderBook::new();
 
-    let side = Side::Buy;
+    book.add_order(Order { id: 1, symbol: String::from("BTC/USDT"), side: Side::Buy, price: 100.0, quantity: 10 });
+    let trades2 = book.add_order(Order { id: 2, symbol: String::from("BTC/USDT"), side: Side::Sell, price: 95.0, quantity: 5 });
+    
+    println!("Trades after second order : {:?}", trades2);
 
-    let o = Order {
-        id : 34,
-        symbol : String::from("BTC/USDT"),
-        side ,
-        price: 45.0,
-        quantity : 100
-    };
+    let trades3 = book.add_order(Order { id: 3, symbol: String::from("BTC/USDT"), side: Side::Buy, price: 105.0, quantity: 5 });
 
-    println!("{:?}", o);
-
+    println!("Trades after third order: {:?}", trades3);
 }
